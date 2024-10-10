36cfc8513d03f2d8e48e2fd3590844eb3e993512
Abdul Moiz <moiza0799@gmail.com>
2024-10-10 16:55:09 -0500

    Added option to view comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,7 @@
+import { useState } from 'react'
 import './App.scss'
 import avatar from './images/bozai.png'
 
-
-// Comment List data
-const defaultList = [
-  {
-    // comment id
-    rpid: 3,
-    // user info
-    user: {
-      uid: '13258165',
-      avatar: '',
-      uname: 'Jay Zhou',
-    },
-    // comment content
-    content: 'Nice, well done',
-    // created datetime
-    ctime: '10-18 08:15',
-    like: 88,
-  },
-  {
-    rpid: 2,
-    user: {
-      uid: '36080105',
-      avatar: '',
-      uname: 'Song Xu',
-    },
-    content: 'I search for you thousands of times, from dawn till dusk.',
-    ctime: '11-13 11:29',
-    like: 88,
-  },
-  {
-    rpid: 1,
-    user: {
-      uid: '30009257',
-      avatar,
-      uname: 'John',
-    },
-    content: 'I told my computer I needed a break... now it will not stop sending me vacation ads.',
-    ctime: '10-19 09:00',
-    like: 66,
-  },
-]
 // current logged in user info
 const user = {
   // userid
@@ -59,6 +19,46 @@ const tabs = [
 ]
 
 const App = () => {
+  const [comments, setComments] = useState([
+    {
+      // comment id
+      rpid: 3,
+      // user info
+      user: {
+        uid: '13258165',
+        avatar: '',
+        uname: 'Jay Zhou',
+      },
+      // comment content
+      content: 'Nice, well done',
+      // created datetime
+      ctime: '10-18 08:15',
+      like: 88,
+    },
+    {
+      rpid: 2,
+      user: {
+        uid: '36080105',
+        avatar: '',
+        uname: 'Song Xu',
+      },
+      content: 'I search for you thousands of times, from dawn till dusk.',
+      ctime: '11-13 11:29',
+      like: 88,
+    },
+    {
+      rpid: 1,
+      user: {
+        uid: '30009257',
+        avatar,
+        uname: 'John',
+      },
+      content: 'I told my computer I needed a break... now it will not stop sending me vacation ads.',
+      ctime: '10-19 09:00',
+      like: 66,
+    },
+  ])
+
   return (
     <div className="app">
       {/* Nav Tab */}
@@ -67,7 +67,7 @@ const App = () => {
           <li className="nav-title">
             <span className="nav-title-text">Comments</span>
             {/* Like */}
-            <span className="total-reply">{10}</span>
+            <span className="total-reply">{comments.length}</span>
           </li>
           <li className="nav-sort">
             {/* highlight class name： active */}
@@ -113,23 +113,30 @@ const App = () => {
             </div>
 
             <div className="content-wrap">
-              {/* username */}
-              <div className="user-info">
-                <div className="user-name">jack</div>
-              </div>
-              {/* comment content */}
-              <div className="root-reply">
-                <span className="reply-content">This is reply</span>
-                <div className="reply-info">
-                  {/* comment created time */}
-                  <span className="reply-time">{'2023-11-11'}</span>
-                  {/* total likes */}
-                  <span className="reply-time">Like:{100}</span>
-                  <span className="delete-btn">
-                    Delete
-                  </span>
-                </div>
-              </div>
+
+              {comments.map(comment => {
+                return (
+                  <>
+                    <div className="user-info">
+                      <div className="user-name">{comment.user.uname}</div>
+                    </div>
+
+                    {/* comment content */}
+                    <div className="root-reply">
+                      <span className="reply-content">{comment.content}</span>
+                      <div className="reply-info">
+                        {/* comment created time */}
+                        <span className="reply-time">{comment.ctime}</span>
+                        {/* total likes */}
+                        <span className="reply-time">Like:{comment.like}</span>
+                        <span className="delete-btn">
+                          Delete
+                        </span>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
             </div>
           </div>
         </div>
